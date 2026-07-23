cae16df0638fdb9feb0478c80aeecc9f9484f024
waqarshah776 <130747546+waqarshah776@users.noreply.github.com>
2026-07-23 13:09:04 +0500

    chore: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-600">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Welcome to BizFlow 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
